8d8a4f9360b21b7edd67927ff38ee0be77b5c210
Tamiflu9 <33600064+Tamiflu9@users.noreply.github.com>
2020-11-15 17:23:10 +0100

    Recursión

---
+++ 23 calcular el número de dígitos/Source.cpp
@@ -0,0 +1,57 @@
+﻿// Nombre del alumno .....
+// Usuario del Juez ......
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+
+void sol(long long int numero, int &c) {
+
+	if (numero > 0) {
+		c++;
+		sol(numero/10, c);
+	}
+ }
+
+
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+bool resuelveCaso() {
+	// leer los datos de la entrada
+	long long int num;
+	std::cin >> num;
+
+	if (!std::cin)
+		return false;
+
+	int cont = 0;
+	sol(num, cont);
+	std::cout << cont << '\n';
+
+
+	return true;
+
+}
+
+int main() {
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif 
+
+
+	while (resuelveCaso());
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
